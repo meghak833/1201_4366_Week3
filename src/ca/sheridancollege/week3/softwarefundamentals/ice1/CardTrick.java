f8f5e54ce57816e2d48c57586d91211006bed360
Emily Diano <59581794+princessefd@users.noreply.github.com>
2020-01-20 18:11:13 -0500

    Update CardTrick.java
    added magic hand print statement

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,6 +1,8 @@
 
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Arrays;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -22,6 +24,8 @@ public class CardTrick {
             c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
         }
 
+        System.out.println("Here is your magic hand!");
+        System.out.println(Arrays.toString(magicHand));
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
