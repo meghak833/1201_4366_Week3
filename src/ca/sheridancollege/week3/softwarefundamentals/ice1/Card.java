9b916190d422928c5ffa7f5e87acc807e396b006
NordChen <noaru443@outlook.com>
2020-01-20 17:10:42 -0500

    Modified Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -6,48 +6,63 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
+ * A class that models playing card Objects. Cards have a value (note that Ace =
+ * 1, Jack -11, Queen =12, King = 13) A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers. This code is to be used in ICE1.
+ * When you create your own branch, add your name as a modifier.
+ *
  * @author Megha Patel
+ * @modifier Zehao Chen
+ * ID: 991583852
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+	private String suit; //clubs, spades, diamonds, hearts
+	private int value;//1-13
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
-    /**
-     * @return the suit
-     */
-    public String getSuit() {
-        return suit;
-    }
+	public Card() {
 
-    /**
-     * @param suit the suit to set
-     */
-    public void setSuit(String suit) {
-        this.suit = suit;
-    }
+	}
 
-    /**
-     * @return the value
-     */
-    public int getValue() {
-        return value;
-    }
+	public Card(String s, int v) {
+		this.suit = s;
+		if (v > 13) {
+			this.value = 13;
+		} else if (v < 1) {
+			this.value = 1;
+		} else {
+			this.value = v;
+		}
+	}
 
-    /**
-     * @param value the value to set
-     */
-    public void setValue(int value) {
-        this.value = value;
-    }
-   
-   
-    
+	public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+
+	/**
+	 * @return the suit
+	 */
+	public String getSuit() {
+		return suit;
+	}
+
+	/**
+	 * @param suit the suit to set
+	 */
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+
 }
