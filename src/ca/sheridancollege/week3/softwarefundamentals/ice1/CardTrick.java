9df00484fe469c6d44297e01d699a5b60788d3c8
mohammed <siddiq33@shernet.sheridancollege.ca>
2020-01-20 16:49:54 -0500

    added address variable

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,23 +5,33 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Random;
+import java.util.Scanner;
+
+
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ * @modifier Mohammed Siddiqui
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        
+        Scanner input= new Scanner(System.in);
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)Math.random());
+            c.setSuit(Card.SUITS[[new Random().nextInt(SUITS.length)]);
+            //Cannot run due to unresolved issue with this line of code.
+            
         }
         
         //insert code to ask the user for Card value and suit, create their card
