82740c171e227b1b7753b913d1280b1c866b6d50
Matt <matt@Matt>
2020-01-20 18:48:40 -0500

    inserted call to random number generator
    inserted code to ask user for card value and suit
    
    inserted code to report results

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,32 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*99*/
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
+ * @modifier Matt Dutra
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
-        Card[] magicHand = new Card[7];
+    	
+    	Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)((Math.random()*13)+1));
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
-    
+        Card c = new C();
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter card value: ");
+        int value = in.nextInt();
+        c.setValue((int)((Math.random()*13)+1));
+        Scanner in2 = new Scanner(System.in);
+        System.out.print("Enter card suit: ");
+        String suit = in2.nextLine();
+        c.setSuit(C.SUITS[(int)(Math.random()*4)]);
+        
+        System.out.println("Your lucky card is the "+c.getValue()+" of "
+        		+c.getSuit());
+    } 
 }
