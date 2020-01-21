591f3dde01c6c44a2202f893041c1c5c71c2614e
hiba <bhuttah@sheridancollege.ca>
2020-01-20 21:45:58 -0500

    initial commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -37,27 +37,6 @@ public class CardTrick {
         String suit = input.nextLine();
         
         
-        Card usersCard = new Card(); //creating object
-        usersCard.setValue(card);
-        usersCard.setSuit(suit);
         
-        
-      switch (card) {
-            case 13:
-                System.out.println("Your card is King of " + suit); //prints out the user input
-                break;
-            case 12:
-                System.out.println("Your card is Queen of " + suit); //prints out the user input
-                break;
-            case 11:
-                System.out.println("Your card is Jack of " + suit); //prints out the user input
-                break;
-            case 1:             
-                System.out.println("Your card is Ace of " + suit); //prints out the user input
-                break;
-            default:
-                System.out.println("Your card is " + card + " of " + suit); //prints out the user input
-                break;
-    }
     }
 }
