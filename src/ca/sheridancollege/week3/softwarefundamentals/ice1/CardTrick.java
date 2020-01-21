0e626bc670929c8d0f29eeb7808c707881a92096
dutra88 <59579238+dutra88@users.noreply.github.com>
2020-01-20 19:19:40 -0500

    Update CardTrick.java
    added comments

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,6 +1,7 @@
 /*991421314*/
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+/** Imports the Scanner utility. */
 import java.util.Scanner;
 
 /**
@@ -25,16 +26,16 @@ public class CardTrick {
         }
         
         Card c = new Card();
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); // creates scanner for the card value
         System.out.print("Enter card value: ");
         int value = in.nextInt();
-        c.setValue((int)((Math.random()*13)+1));
-        Scanner in2 = new Scanner(System.in);
+        c.setValue((int)((Math.random()*13)+1)); // sets the random value for the card value
+        Scanner in2 = new Scanner(System.in); // creates scanner for the card suit
         System.out.print("Enter card suit: ");
         String suit = in2.nextLine();
-        c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+        c.setSuit(Card.SUITS[(int)(Math.random()*4)]); // sets the random suit for the card suit
         
-        System.out.println("Your lucky card is the "+c.getValue()+" of "
+        System.out.println("Your lucky card is the "+c.getValue()+" of " // prints your lucky card
         		+c.getSuit());
     } 
 }
