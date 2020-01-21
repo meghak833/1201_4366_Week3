b3ad4bbbd4cac7aeef5f458cfef06441590c447b
mamounjr1 <47333746+mamounjr1@users.noreply.github.com>
2020-01-20 23:53:43 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -56,6 +56,9 @@ public class CardTrick {
              
          }
          
+            
+            System.out.println(c.getValue());
+            System.out.println(c.getSuit());
  
                
            
