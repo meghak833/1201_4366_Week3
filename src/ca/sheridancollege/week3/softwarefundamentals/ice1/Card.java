65af5a3bfe6ce34069a4e94ed7359620c47a33a0
Tyler <makrist@sheridancollege.ca>
2020-01-20 23:20:16 -0500

    Added final touches

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -14,6 +14,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * add your name as a modifier.
  * @author Megha Patel
  * @Modifier: Tyler Makris
+ * @Student #: 991571812
  */
 public class Card {
 
@@ -49,6 +50,8 @@ public class Card {
         this.value = value;
     }
    
-   
+   public String toString(){
+      return "Card value is " + getValue() + ". Suit value is " + getSuit();
+   }
     
 }
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,29 +10,53 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha Patel
- * @Student #:991571812
+ * @Modifier Tyler Makris
+ * @Student #: 991571812
  */
+import java.lang.Math;
 import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-        int cardValue = 0;
-        int suitNum = 0;
-        Card[] magicHand = new Card[7];
-        Card[] userHand = new Card[1];
+        Scanner sc = new Scanner(System.in); // Used for user input
+        int cardValue = 0; // Card value
+        int suitNum = 0; // Suit value
+        Card[] magicHand = new Card[7]; // Mystery hand
+        Card[] userHand = new Card[1]; // User hand
+        boolean ifFound = false; // Used to check if users card is in deck
         
+        // Sets the magicHand with random numbers and suits
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            c.setValue((int) (Math.random() + 11));
-            c.setSuit(Card.SUITS[(int) (Math.random() + 3)]);
+            c.setValue((int)(Math.random() * 13));
+            c.setSuit(Card.SUITS[(int)(Math.random() * 4)]);
+            System.out.println(c.toString());
+            magicHand[i] = c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+       //User creates card here
+       Card d = new Card();
+       System.out.print("Enter a card value: ");
+       d.setValue(sc.nextInt());
+       System.out.print("Enter suit value: ");
+       d.setSuit(Card.SUITS[sc.nextInt()]);
+       userHand[0] = d;
+      
+       //Results are displayed
+       for(int i = 0; i< magicHand.length; i++) {
+           if(userHand[0].toString().equals(magicHand[i].toString())){
+               System.out.println("Your card was found!");
+               ifFound = true;
+               break;
+           }
+       }
+       
+       if(ifFound == true){
+           System.out.println("Card was not in deck");
+       }
+        
     }
     
 }
