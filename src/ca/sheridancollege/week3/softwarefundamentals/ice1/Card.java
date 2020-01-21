f1fc28332a9ed0a9e96c759b5a6f2532fbe113ef
Hanna411 <hanzh@sheridancollege.ca>
2020-01-20 22:20:55 -0500

    Final version

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -43,9 +43,11 @@ public class Card {
      */
     public void setValue(int value) {
         this.value = value;
+    }
+        
     @Override
     public String toString() {
         return "The card number is " + this.value + " and card suit is " + this.suit;
     }
-  } 
+   
 }
